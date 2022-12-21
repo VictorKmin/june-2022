0681d39d0afcaebbb0700dfcbdbfbf76518a2e1f
VictorKmin <kminvictor@gmail.com>
2022-12-22 01:30:17 +0200

    Socket IO

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const swaggerUI = require('swagger-ui-express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 
 const userRouter = require('./router/user.router');
 const authRouter = require('./router/auth.router');
@@ -11,12 +13,51 @@ const { cronRunner } = require('./cron');
 const swaggerJson = require('./swagger.json');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('static'));
 
 app.use(fileUpload());
+
+const io = socketIO(server, { cors: 'http://localhost:80' });
+
+io.on('connection', (socket) => {
+  console.log(socket);
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} was dssconected`)
+  });
+
+  // console.log(socket.id);
+  //
+  // console.log(socket.handshake.auth);
+  // console.log(socket.handshake.query);
+
+  socket.on('message:send', (messageData) => {
+    console.log(messageData.text);
+
+    // SEND ONE TO ONE EVENT
+    // socket.emit('message:new', messageData.text);
+
+    // SEND EVENT TO ALL EXCEPT EMITTER
+    // socket.broadcast.emit('message:new', messageData.text);
+
+    // SEND EVENT TO ALL CLIENTS
+    io.emit('message:new', messageData.text);
+  })
+
+  socket.on('room:join', (roomInfo) => {
+    socket.join(roomInfo.roomId); // SOCKET JOIN ROOM
+    // socket.leave(roomInfo.roomId); // SOCKET LEAVE ROOM
+
+    // SEND TO ALL IN ROOM EXCEPT NEW MEMBER
+    // socket.to(roomInfo.roomId).emit('user:room:join', socket.id);
+
+    // SEND TO ALL ROOM MEMBERS
+    io.to(roomInfo.roomId).emit('user:room:join', socket.id);
+  });
+});
 
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
@@ -33,7 +74,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
   await mongoose.connect('mongodb://localhost:27017/june2022');
   console.log(`Server listen ${configs.PORT}`);
   // cronRunner();
